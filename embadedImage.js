4e9b3fe7fe08828336eedf96a2cea31bb82262fd
Juturu <djuturu@kpmg.com.au>
2024-09-23 16:07:55 +1000

    update code for with image

--- embadedImage.js
+++ embadedImage.js
@@ -1,36 +1,35 @@
 const express = require("express");
-// const QRCode = require('qrcode');
 const app = express();
 const { QRCodeCanvas } = require("@loskir/styled-qr-code-node");
 const path = require("path");
-const fs = require("fs");
+const multer = require('multer');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from 'public' folder for frontend
 app.use(express.static("public"));
+const upload = multer({ dest: 'uploads/' });
 
 // Serve index.html on root '/'
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "UrlGenerate.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "UrlGenerate.html"));
+// });
 // QR Code generation endpoint
-app.post("/generateurl", async (req, res) => {
+app.post("/generateurl", upload.single('logo') ,async (req, res) => {
   const { data } = req.body;
+  const logoFilePath = req.file.path;
 
   if (!data) {
     return res.status(400).json({ error: "No data provided" });
   }
-
   try {
     const qrCode = new QRCodeCanvas({
       data: data,
-      image: path.join(__dirname, "public", "IDY Logo.png"), // Path to the logo
+      image: logoFilePath, // Path to the logo
     });
-    // await qrCode.toFile("./public/output.png", "png");
     // Define the output file path
-    const outputFilePath = path.join(__dirname, "public", "qr_with_logo.png");
+    const outputFilePath = path.join(__dirname, "public","qrcodes", "qr_with_logo.png");
     await qrCode.toFile(outputFilePath);
 
     res.sendFile(outputFilePath);
