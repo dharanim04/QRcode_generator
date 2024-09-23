aa63d3626ba6db2463beb2b883693c6266b9e234
Juturu <djuturu@kpmg.com.au>
2024-09-23 16:14:06 +1000

    updated index page with all functionalities

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const path = require('path');
-const QRCode = require('qrcode');
+const express = require("express");
+const path = require("path");
+const QRCode = require("qrcode");
 const app = express();
+const { QRCodeCanvas } = require("@loskir/styled-qr-code-node");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from 'public' folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Serve index.html on root '/'
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// QR Code generation endpoint
-app.post('/generate', async (req, res) => {
-  const { firstname, lastname, mobileNumber, address } =req.body;
+// QR Code generation endpoint contactcard
+app.post("/generateContactCard", async (req, res) => {
+  const { firstname, lastname, mobileNumber, address } = req.body;
   if (!firstname || !mobileNumber) {
-    return res.status(400).json({ error: 'First name and last name are required' });
+    return res
+      .status(400)
+      .json({ error: "First name and last name are required" });
   }
 
-  const vCard =` BEGIN:VCARD
+  const vCard = ` BEGIN:VCARD
 VERSION:3.0
 N:${lastname};${firstname}
 FN:${firstname} ${lastname}
@@ -34,27 +40,68 @@ URL:
 END:VCARD`;
 
   //working
-//   const vCard =` BEGIN:VCARD
-// VERSION:3.0
-// N:test2;dtest
-// FN:dtest test2
-// ADR:;;;;;;
-// TEL;WORK;VOICE:
-// TEL;CELL:4566sdf
-// TEL;FAX:
-// EMAIL;WORK;INTERNET:
-// URL:
-// END:VCARD`;
+  //   const vCard =` BEGIN:VCARD
+  // VERSION:3.0
+  // N:test2;dtest
+  // FN:dtest test2
+  // ADR:;;;;;;
+  // TEL;WORK;VOICE:
+  // TEL;CELL:4566sdf
+  // TEL;FAX:
+  // EMAIL;WORK;INTERNET:
+  // URL:
+  // END:VCARD`;
 
   try {
     // Generate QR code
     const qrCode = await QRCode.toDataURL(vCard.trim());
     res.json({ qrCode });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to generate QR code' });
+    res.status(500).json({ error: "Failed to generate QR code" });
   }
 });
 
+// QR Code generation endpoint with image
+app.post("/generateQRwithImage", upload.single('logo') ,async (req, res) => {
+  const { data } = req.body;
+  const logoFilePath = req.file.path;
+
+  if (!data) {
+    return res.status(400).json({ error: "No data provided" });
+  }
+  try {
+    const qrCode = new QRCodeCanvas({
+      data: data,
+      image: logoFilePath, // Path to the logo
+    });
+    // Define the output file path
+    const outputFilePath = path.join(__dirname, "public","qrcodes", "qr_with_logo.png");
+    await qrCode.toFile(outputFilePath);
+
+    res.sendFile(outputFilePath);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ error: "Failed to generate QR code" });
+  }
+});
+
+
+// QR Code generation endpoint
+app.post('/generateurl', async (req, res) => {
+  const { data } = req.body;
+
+  if (!data) {
+    return res.status(400).json({ error: 'No data provided' });
+  }
+
+  try {
+    // Generate QR code
+    const qrCode = await QRCode.toDataURL(data);
+    res.json({ qrCode });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to generate QR code' });
+  }
+});
 // Start the server
 const port = 3000;
 app.listen(port, () => {
